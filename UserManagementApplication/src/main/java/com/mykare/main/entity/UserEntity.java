9a2a2c1b9ae1fba2a259e40d91ecc266587d4fdf
rajivsuting <rajivsuting16@gmail.com>
2023-07-20 11:39:33 +0530

    task completed

---
+++ UserManagementApplication/src/main/java/com/mykare/main/entity/UserEntity.java
@@ -0,0 +1,40 @@
+package com.mykare.main.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class UserEntity {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	@Column(unique = true)
+	private String email;
+	
+	@JsonProperty(access = Access.WRITE_ONLY)
+	private String password;
+	
+	private String role;
+
+}
